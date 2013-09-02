8e1db4673ab30c9bc5a2462e04d1d38f021f46b9
bobo <bobo@servicezon.com>
2013-09-02 14:38:34 +0800

    update confirm

---
+++ core/src/main/java/org/sz/platform/bpm/engine/SzProcessEngineConfiguration.java
@@ -0,0 +1,8 @@
+package org.sz.platform.bpm.engine;
+
+import org.activiti.spring.SpringProcessEngineConfiguration;
+
+public class SzProcessEngineConfiguration extends
+		SpringProcessEngineConfiguration {
+
+}
